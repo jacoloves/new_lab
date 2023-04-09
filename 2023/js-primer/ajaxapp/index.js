ad73b2f33e2d59463de7163add6f9c02c303581e
jacoloves <5511068t@gmail.com>
2023-04-10 00:29:41 +0900

    js-primer http request

--- 2023/js-primer/ajaxapp/index.js
+++ 2023/js-primer/ajaxapp/index.js
@@ -6,3 +6,20 @@ const button = document.createElement("button");
 button.textContent = "Push Me";
 
 document.body.appendChild(button);
+
+// const userId = "jacoloves";
+function fetchUserInfo(userId) {
+    fetch( `https://api.github.com/users/${encodeURIComponent(userId)}`)
+        .then(response => {
+            console.log(response.status);
+            if (!response.ok) {
+                console.error("error response", response);
+            } else {
+                return response.json().then(userInfo => {
+                    console.log(userInfo);
+                });
+            }
+        }).catch(error => {
+            console.error(error);
+        });
+}
