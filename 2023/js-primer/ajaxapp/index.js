a0a531c2e5687ba3f2e9c75cab7e036eca4aabc1
jacoloves <5511068t@gmail.com>
2023-04-10 00:13:58 +0900

    entry-pointjs primer

---
+++ 2023/js-primer/ajaxapp/index.js
@@ -0,0 +1,8 @@
+const heading = document.querySelector("h2");
+
+const headingText = heading.textContent;
+
+const button = document.createElement("button");
+button.textContent = "Push Me";
+
+document.body.appendChild(button);
