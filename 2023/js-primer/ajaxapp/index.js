e439b33e9b5fcdbadaa125debb553f109ed4b698
jacoloves <5511068t@gmail.com>
2023-04-22 20:18:27 +0900

    20230422

--- 2023/js-primer/ajaxapp/index.js
+++ 2023/js-primer/ajaxapp/index.js
@@ -1,3 +1,4 @@
+/*
 const heading = document.querySelector("h2");
 
 const headingText = heading.textContent;
@@ -6,20 +7,69 @@ const button = document.createElement("button");
 button.textContent = "Push Me";
 
 document.body.appendChild(button);
+*/
+
+async function main() {
+    try {
+        const userId = getUserId();
+        const userInfo = await fetchUserInfo(userId);
+        const view = createView(userInfo);
+        displayView(view);
+    } catch(error) {
+        console.error(`error has occured (${error})`);
+    }
+}
 
 // const userId = "jacoloves";
 function fetchUserInfo(userId) {
-    fetch( `https://api.github.com/users/${encodeURIComponent(userId)}`)
+    return fetch( `https://api.github.com/users/${encodeURIComponent(userId)}`)
         .then(response => {
-            console.log(response.status);
             if (!response.ok) {
-                console.error("error response", response);
+                return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
             } else {
-                return response.json().then(userInfo => {
-                    console.log(userInfo);
-                });
+                return response.json();
             }
-        }).catch(error => {
-            console.error(error);
         });
+}
+
+function getUserId() {
+    return document.getElementById("userId").value;
+}
+
+function createView(userInfo) {
+    return escapeHTML`
+    <h4>${userInfo.name} (@${userInfo.login})</h4>
+    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+    <dl>
+        <dt>Location</dt>
+        <dd>${userInfo.location}</dd>
+        <dt>Repositories</dt>
+        <dd>${userInfo.public_repos}</dd>
+    </dl>
+    `;
+}
+
+function displayView(view) {
+    const result = document.getElementById("result");
+    result.innerHTML = view;
+}
+
+function escapeSpecialChars(str) {
+    return str
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
+
+function escapeHTML(strings, ...values) {
+    return strings.reduce((result, str, i) => {
+        const value = values[i-1];
+        if (typeof value === "string") {
+            return result + escapeSpecialChars(value) + str;
+        } else {
+            return result + String(value) + str;
+        }
+    });
 }
