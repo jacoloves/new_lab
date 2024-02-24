51f5d1b2a37daabe4f00e6434431ea25aff2b883
shooonng <5511068t@gmail.com>
2024-02-25 01:14:56 +0900

    20240225 haskell

---
+++ 2024/introductory-haskell-programming/chap2/q2-2.hs
@@ -0,0 +1,9 @@
+inc :: Int -> Int
+inc n = n + 1
+
+double :: Int -> Int
+double n = n * 2
+
+square :: Int -> Int
+square n = n ^ 2
+
---
+++ 2024/introductory-haskell-programming/chap2/q2-3.hs
@@ -0,0 +1,3 @@
+ex3 n = if n `mod` 2 == 0
+           then n - 2
+           else 3*n+1
