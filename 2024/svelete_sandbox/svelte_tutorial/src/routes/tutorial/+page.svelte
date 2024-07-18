46690500dadd00305ffb410bbe738c9a96ae95b0
shooonng <5511068t@gmail.com>
2024-07-18 23:56:35 +0900

    styling

--- 2024/svelete_sandbox/svelte_tutorial/src/routes/tutorial/+page.svelte
+++ 2024/svelete_sandbox/svelte_tutorial/src/routes/tutorial/+page.svelte
@@ -1,1 +1,9 @@
-<h1>Welcome!</h1>
+<p>This is a paragraph.</p>
+
+<style>
+    p {
+        color: goldenrod;
+        font-family: 'Comic Sans MS', cursive;
+        font-size: 2em;
+    }
+</style>
