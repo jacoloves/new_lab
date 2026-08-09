a20c0a2524d91d53ebd9554e5d0e175ba8642818
shooonng <5511068t@gmail.com>
2026-08-09 11:55:40 +0900

    20260809 WIP: ecs-book chap5

--- 2026/ecs-begginer-book/chap5/db.tf
+++ 2026/ecs-begginer-book/chap5/db.tf
@@ -11,3 +11,56 @@ resource "aws_db_subnet_group" "main" {
     Name = "sbcntr-main"
   }
 }
+
+resource "aws_rds_cluster" "main" {
+  cluster_identifier = "sbcntr-main"
+  engine             = "aurora-postgresql"
+
+  engine_lifecycle_support = "open-source-rds-extended-support-disabled"
+
+  database_name   = "app"
+  master_username = "sbcntradmin"
+
+  manage_master_user_password = true
+
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.db.id]
+  port                   = 5432
+
+  enable_http_endpoint = true
+
+  serverlessv2_scaling_configuration {
+    min_capacity             = 0
+    max_capacity             = 4
+    seconds_until_auto_pause = 300
+  }
+
+  backup_retention_period = 1
+  storage_encrypted       = true
+
+  deletion_protection = true
+
+  preferred_maintenance_window = "sat:12:00-sat:12:30"
+
+  skip_final_snapshot = true
+
+  tags = {
+    Name = "sbcntr-main"
+  }
+}
+
+resource "aws_rds_cluster_instance" "main" {
+  identifier         = "sbcntr-main-instance-1"
+  cluster_identifier = aws_rds_cluster.main.id
+
+  instance_class = "db.serverless"
+  engine         = aws_rds_cluster.main.engine
+  engine_version = aws_rds_cluster.main.engine_version
+
+  publicly_accessible        = false
+  auto_minor_version_upgrade = true
+
+  tags = {
+    Name = "sbcntr-main-instance-1"
+  }
+}
